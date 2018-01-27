97844f883b245fd740150351905d58a3ee2fc3ca
Return Null <0returnnull0@gmail.com>
2018-01-27 16:59:39 +0100

    1

--- JS/animation.js
+++ JS/animation.js
@@ -1,18 +1,28 @@
 function animate() {
   strokeWidth(4);
-  strokeColor("#55a");
+  strokeColor("#a00");
   fillColor("white");
 
   for(var i = 0; i < 10; i++) {
-    fillCircle(window.innerWidth / 3, window.innerHeight / 2 - 80 * 5 + 80 * i - 15, 30);
-    strokeCircle(window.innerWidth / 3, window.innerHeight / 2 - 80 * 5 + 80 * i - 15, 30);
+    for(var j = 0; j < 5; j++) {
+      line(window.innerWidth / 4, window.innerHeight / 2 - 80 * 5 + 80 * i - 15, window.innerWidth / 4 * 2, window.innerHeight / 2 - 80 * 2.5 + 80 * j - 15);
+      if(i == 0)
+        line(window.innerWidth / 4 * 2, window.innerHeight / 2 - 80 * 2.5 + 80 * j - 15, window.innerWidth / 4 * 3, window.innerHeight / 2 - 15);
+    }
+  }
+
+  strokeColor("#55a");
+
+  for(var i = 0; i < 10; i++) {
+    fillCircle(window.innerWidth / 4, window.innerHeight / 2 - 80 * 5 + 80 * i - 15, 30);
+    strokeCircle(window.innerWidth / 4, window.innerHeight / 2 - 80 * 5 + 80 * i - 15, 30);
   }
 
   for(var i = 0; i < 5; i++) {
-    fillCircle(window.innerWidth / 3, window.innerHeight / 2 - 80 * 2.5 + 80 * i - 15, 30);
-    strokeCircle(window.innerWidth / 3, window.innerHeight / 2 - 80 * 2.5 + 80 * i - 15, 30);
+    fillCircle(window.innerWidth / 4 * 2, window.innerHeight / 2 - 80 * 2.5 + 80 * i - 15, 30);
+    strokeCircle(window.innerWidth / 4 * 2, window.innerHeight / 2 - 80 * 2.5 + 80 * i - 15, 30);
   }
 
-  fillCircle(window.innerWidth / 3, window.innerHeight / 2 - 15, 30);
-  strokeCircle(window.innerWidth / 3, window.innerHeight / 2 - 15, 30);
+  fillCircle(window.innerWidth / 4 * 3, window.innerHeight / 2 - 15, 30);
+  strokeCircle(window.innerWidth / 4 * 3, window.innerHeight / 2 - 15, 30);
 }
